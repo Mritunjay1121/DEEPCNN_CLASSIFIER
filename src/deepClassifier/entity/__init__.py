f75dbcd49e59b61d7c1ec3db0638a364804eaf01
Mritunjay1121 <mritunjay.1121@gmail.com>
2022-11-14 14:13:38 +0530

    Stage 2 added to deepclassifier files

--- src/deepClassifier/entity/__init__.py
+++ src/deepClassifier/entity/__init__.py
@@ -1,5 +1,1 @@
-from deepClassifier.entity.config_entity import DataIngestionConfig
-# from pathlib import Path
-
-# CONFIG_FILE_PATH=Path("configs/config.yaml")
-# PARAMS_FILE_APTH = Path("params.yaml")
+from deepClassifier.entity.config_entity import DataIngestionConfig,PrepareBaseModelConfig
