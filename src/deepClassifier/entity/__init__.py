94b582f37e1c8f8ec2471932b9326a5cabe84e1f
Mritunjay1121 <mritunjay.1121@gmail.com>
2022-11-28 22:49:23 +0530

    Evaluation stage files with ml flow

--- src/deepClassifier/entity/__init__.py
+++ src/deepClassifier/entity/__init__.py
@@ -2,7 +2,8 @@ from deepClassifier.entity.config_entity import (
     DataIngestionConfig,
     PrepareBaseModelConfig,
     PrepareCallbacksConfig,
-    TrainingConfig
+    TrainingConfig,
+    EvaluationConfig
 
 
 )
