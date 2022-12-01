abd52c613f3880dd31c752aff4060ad0b09aa440
Mritunjay1121 <mritunjay.1121@gmail.com>
2022-12-01 15:45:30 +0530

    Stage 4 evaluation all files

--- src/deepClassifier/entity/config_entity.py
+++ src/deepClassifier/entity/config_entity.py
@@ -44,7 +44,5 @@ class TrainingConfig:
 class EvaluationConfig:
     path_of_model: Path
     training_data: Path
-    all_params: dict
-    mlflow_uri: str
     params_image_size: list
     params_batch_size: int
